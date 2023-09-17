b70ae1004ae61d8a3af183f560c445926d9396b7
Leomar Bucud <leomar@floriusflowers.com>
2023-09-17 13:49:52 +0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var editor = CodeMirror.fromTextArea(document.getElementById('editor'), {
+    mode: "javascript",
+    lineNumbers: true,
+    theme: 'monokai',
+});
+// editor.setOptions('theme', )
+// editor.save()

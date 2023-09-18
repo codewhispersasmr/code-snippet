7b6edc4cca65c98289da51045d8094a1f531ef89
Leomar Bucud <leomar@floriusflowers.com>
2023-09-18 16:43:36 +0400

    add language selector

--- script.js
+++ script.js
@@ -9,11 +9,18 @@ var editor = CodeMirror.fromTextArea(document.getElementById('editor'), {
 document.onclick = async event => {
     const c = document.querySelector('body');
 
-    domtoimage.toJpeg(c, { quality: 0.95 })
-    .then(function (dataUrl) {
-        var link = document.createElement('a');
-        link.download = 'my-image-name.jpeg';
-        link.href = dataUrl;
-        link.click();
-    });
+    // domtoimage.toJpeg(c, { quality: 0.95 })
+    // .then(function (dataUrl) {
+    //     var link = document.createElement('a');
+    //     link.download = 'my-image-name.jpeg';
+    //     link.href = dataUrl;
+    //     link.click();
+    // });
+}
+
+language.onchange = event => {
+    const lang = language.value;
+    console.log(lang)
+    editor.setOption('mode', lang);
+    editor.save();
 }

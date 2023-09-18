675e452a47562a6ab24755b853c14456785fc1b5
Leomar Bucud <leomar@floriusflowers.com>
2023-09-18 07:55:58 +0400

    Update

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ var editor = CodeMirror.fromTextArea(document.getElementById('editor'), {
 });
 // editor.setOptions('theme', )
 // editor.save()
+
+document.onclick = async event => {
+    const c = document.querySelector('body');
+    html2canvas(c, { allowTaint: true}).then( canvas => {
+        // document.body.appendChild(canvas);
+       // var link = document.createElement("a");
+       //  document.body.appendChild(link);
+       //  link.download = "html_image.jpg";
+       //  link.href = canvas.toDataURL();
+       //  link.target = '_blank';
+       //  link.click(); 
+    });
+}

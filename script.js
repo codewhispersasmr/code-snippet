68faaa568eb18aee49d032199e82ebadb08c68ab
Leomar Bucud <leomar@floriusflowers.com>
2023-09-24 09:22:02 +0400

    Updates

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
 var editor = CodeMirror.fromTextArea(document.getElementById('editor'), {
     mode: "javascript",
-    lineNumbers: true,
+    lineNumbers:  false,
     theme: 'monokai',
+    lineWrapping: true,
 });
-// editor.setOptions('theme', )
-// editor.save()
+download.onclick = async event => {
+    const c = document.querySelector('.snippet-wrapper');
 
-document.onclick = async event => {
-    const c = document.querySelector('body');
-
-    // domtoimage.toJpeg(c, { quality: 0.95 })
-    // .then(function (dataUrl) {
-    //     var link = document.createElement('a');
-    //     link.download = 'my-image-name.jpeg';
-    //     link.href = dataUrl;
-    //     link.click();
-    // });
+    domtoimage.toJpeg(c, { quality: 0.95 })
+    .then(function (dataUrl) {
+        var link = document.createElement('a');
+        link.download = 'my-image-name.jpeg';
+        link.href = dataUrl;
+        link.click();
+    });
 }
 
-language.onchange = event => {
-    const lang = language.value;
-    console.log(lang)
-    editor.setOption('mode', lang);
-    editor.save();
-}
+// language.onchange = event => {
+//     const lang = language.value;
+//     console.log(lang)
+//     editor.setOption('mode', lang);
+//     editor.save();
+// }

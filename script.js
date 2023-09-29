6b98b4ba4bad99a95e43f830e6dbd92601332b9e
Leomar Bucud <leomar@floriusflowers.com>
2023-09-29 18:34:40 +0400

    Updates

--- script.js
+++ script.js
@@ -5,13 +5,14 @@ var editor = CodeMirror.fromTextArea(document.getElementById('editor'), {
     lineWrapping: true,
     scrollbarStyle: 'null'
 });
-download.onclick = async event => {
+
+download.onclick = async () => {
     const c = document.querySelector('.snippet-wrapper');
 
     domtoimage.toJpeg(c, { quality: 0.95 })
     .then(function (dataUrl) {
         var link = document.createElement('a');
-        link.download = 'code.jpg';
+        link.download = 'code.jpeg';
         link.href = dataUrl;
         link.click();
     });
